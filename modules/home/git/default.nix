c4406f5f28a1cd3dd2e20c12ef323758b1476d01
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2024-03-11 14:07:14 -0400

    update git config

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -3,12 +3,43 @@
   programs.git = {
     enable = true;
     
-    userName = "Frost-Phoenix";
-    userEmail = "67cyril6767@gmail.com";
-    
-    extraConfig = { 
-      init.defaultBranch = "main";
-      credential.helper = "store";
+    userName = "Nikita Shumeiko";
+    userEmail = "nikita.shumeiko@vitalbio.com";
+    aliases = {
+      p = "pull";
+      pr = "pull --rebase";
+      psh = "push";
+      ci = "commit";
+      ca = "commit --amend";
+      cim = "commit -m";
+      s = "status";
+      st = "stash";
+      pfwl = "push --force-with-lease";
+      co = "checkout";
+	    cob = "checkout -b";
+      r = "rebase";
+      rbi = "rebase -i";
+      a = "add";
+      sp = "stash pop";
+      cp = "cherry-pick";
+      l = "log";
+      d = "diff";
+      rl = "reflog";
+      m = "merge";
+    };
+    extraConfig = {
+      core.editor = "$EDITOR";
+      core.pager = "${pkgs.delta}/bin/delta";
+      interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
+      add.interactive.useBuiltin = false;
+      delta = {
+          features = "chameleon";
+          side-by-side = false;
+          navigate = true;
+          light = false;
+      };
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
     };
   };
 
