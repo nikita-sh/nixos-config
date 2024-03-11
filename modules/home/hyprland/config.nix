b48de43044c1743ee644ccbdc01c991c1fb0d735
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2024-03-10 23:39:27 -0400

    update hypr config

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   wayland.windowManager.hyprland = {
-    extraConfig = "
+    extraConfig = ''
       $mainMod = SUPER
 
       monitor=,preferred,auto,auto
@@ -19,122 +19,82 @@
       exec-once = waybar &
       exec-once = mako &
 
-      input {
-        kb_layout = us
-        numlock_by_default = false
-        follow_mouse = 1
-        sensitivity = 0
-      }
-
-      misc {
-        disable_autoreload = true
-        disable_hyprland_logo = true
-        always_follow_on_dnd = true
-        layers_hog_keyboard_focus = true
-        animate_manual_resizes = false
-        enable_swallow = true
-        # swallow_regex =
-        focus_on_activate = true
-      }
-
-      general {
-        layout = dwindle
-
-        gaps_in = 0
-        gaps_out = 0
-        border_size = 1
-        col.active_border = rgb(458588)
-        col.inactive_border = rgba(595959aa)
-        # col.active_border = rgb(cba6f7) rgb(94e2d5) 45deg
-        # col.inactive_border = 0x00000000
-        # border_part_of_window = true
-
-        # gaps_in = 5
-        # gaps_out = 10
-        # border_size = 2
-        # col.active_border = rgb(cba6f7) rgb(94e2d5) 45deg
-        # col.inactive_border = 0x00000000
-        # border_part_of_window = false
-      }
-
+      
       xwayland {
-        force_zero_scaling = true
+        force_zero_scaling = false
       }
-
-      dwindle {
-        no_gaps_when_only = false
-        force_split = 0
-        special_scale_factor = 1.0
-        split_width_multiplier = 1.0
-        use_active_for_splits = true
-        pseudotile = yes
-        preserve_split = yes
+      
+      input {
+          kb_layout = us
+          kb_options = grp:win_space_toggle
+          follow_mouse = 1
+          repeat_rate = 30
+          repeat_delay = 500
       }
-
-      master {
-        new_is_master = true
-        special_scale_factor = 1
-        no_gaps_when_only = false
+      
+      general {
+        gaps_in = 5
+        gaps_out = 10
+        border_size = 2
+      	col.active_border = rgb(85796f)
+        col.inactive_border = rgb(504742)
+      	no_cursor_warps = true
+      	resize_on_border = false
+        layout = dwindle
+      	allow_tearing = true
       }
-
+      
       decoration {
-        rounding = 5
-        # rounding = 12
-        
-        active_opacity = 0.90;
-        inactive_opacity = 0.90;
-        fullscreen_opacity = 1.0;
-
-        blur {
-          enabled = true
-
-          size = 4
-          passes = 2
-          
-          brightness = 1
-          contrast = 1.300000
-          ignore_opacity = true
-          noise = 0.011700
-          
-          new_optimizations = true
-          
-          xray = true
-        }
-
-        drop_shadow = true;
-        
-        shadow_ignore_window = true;
-        shadow_offset = 0 2
-        shadow_range = 20
-        shadow_render_power = 3
-        col.shadow = rgba(00000055)
+          rounding = 0
+          blur {
+      		enabled = false
+              size = 10
+              passes = 3
+      		ignore_opacity = true
+          }
+          drop_shadow = yes
+          shadow_range = 17
+      	shadow_scale = 0.98
+      	shadow_offset = 5 3
+          shadow_render_power = 1
+      	shadow_ignore_window = true
+      	col.shadow = rgb(313131)
+      
+      	dim_inactive = true
+      	dim_strength = 0.10
+      	dim_special = 0.3
+      
       }
-
-
-      animations {
-        enabled = true
-        
-        bezier = fluent_decel, 0, 0.2, 0.4, 1
-        bezier = easeOutCirc, 0, 0.55, 0.45, 1
-        bezier = easeOutCubic, 0.33, 1, 0.68, 1
-        bezier = easeinoutsine, 0.37, 0, 0.63, 1
-
-        # Windows
-        animation = windowsIn, 1, 3, easeOutCubic, popin 30% # window open
-        animation = windowsOut, 1, 3, fluent_decel, popin 70% # window close.
-        animation = windowsMove, 1, 2, easeinoutsine, slide # everything in between, moving, dragging, resizing.
-        
-        # Fade
-        animation = fadeIn, 1, 3, easeOutCubic  # fade in (open) -> layers and windows
-        animation = fadeOut, 1, 2, easeOutCubic # fade out (close) -> layers and windows
-        animation = fadeSwitch, 0, 1, easeOutCirc # fade on changing activewindow and its opacity
-        animation = fadeShadow, 1, 10, easeOutCirc # fade on changing activewindow for shadows
-        animation = fadeDim, 1, 4, fluent_decel # the easing of the dimming of inactive windows
-        animation = border, 1, 2.7, easeOutCirc # for animating the border's color switch speed
-        animation = borderangle, 1, 30, fluent_decel, once # for animating the border's gradient angle - styles: once (default), loop
-        animation = workspaces, 1, 4, easeOutCubic, fade # styles: slide, slidevert, fade, slidefade, slidefadevert
+      
+      animations { 
+      	enabled = no
+      	bezier = workspace,0.17, 1.17, 0.3,1
+      	bezier = windowin,0.15,1.15,0.95,1
+      	bezier = windowmove,0.7, 0.7, 0.1,1.05
+          animation = windows, 1, 3, default, popin
+          animation = windowsIn, 1, 3.2, windowin, popin 95%
+          animation = windowsOut, 1, 4, default, popin 96%
+          animation = windowsMove, 1, 4, windowmove, slide
+          animation = workspaces, 1, 5, workspace, slidefadevert 8%
+      	animation = specialWorkspace, 1, 5, workspace, slidefadevert 10%
+      	# first_launch_animation = true
       }
-
+      
+      
+      binds {
+          allow_workspace_cycles = true
+      }
+      
+      misc {
+          disable_hyprland_logo = true
+          animate_manual_resizes = true
+          disable_autoreload = false 
+      	enable_swallow = true
+      	animate_mouse_windowdragging = true
+      	layers_hog_keyboard_focus = true
+      	no_direct_scanout = true
+      	vrr = 0
+      }
 
       # ----------------------------------------------------------------
       
@@ -279,6 +239,6 @@
       windowrulev2 = float,title:^(branchdialog)$
       windowrulev2 = float,title:^(Confirm to replace files)$
       windowrulev2 = float,title:^(File Operation Progress)$
-    ";
+    '';
   };
 }
