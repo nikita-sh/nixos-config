b789e3fde801a6bbb74f952cda30b7d26e92e284
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2024-04-24 13:50:00 -0400

    fix spotify bind

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -131,7 +131,7 @@
       bind =,Cancel,exec,playerctl next
       # bind = $mainMod SHIFT, S, exec, grimshot copy window
       bind = $mainMod SHIFT, C, exec, pkill cycle_wallpaper && ~/dev/nixos-config/wallpapers/cycle_wallpaper.sh &
-      bind = $mainMod SHIFT, M, exec, ELECTRON_OZONE_PLATFORM_HINT=wayland hyprctl dispatch exec '[workspace 5 silent]' spotify
+      bind = $mainMod SHIFT, M, exec, ELECTRON_OZONE_PLATFORM_HINT=wayland hyprctl dispatch exec '[workspace 4 silent] spotify --enable-features=UseOzonePlatform --ozone-platform=wayland'
       
       # screenshot
       bind = $mainMod, Print, exec, grimblast --notify --cursor save area ~/Pictures/$(date +'%Y-%m-%d-At-%Ih%Mm%Ss').png
