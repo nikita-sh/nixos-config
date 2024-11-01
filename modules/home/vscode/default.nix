d0969be4eecf055f7ad81b0ca20dfc69ea672826
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2024-11-01 10:12:01 -0400

    update vscode settings

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -23,15 +23,43 @@
       zxh404.vscode-proto3
     ];
     userSettings = {
+      "editor.fontFamily" = "'FiraCode Nerd Font', Menlo, Monaco, 'Courier New', monospace";
+      "editor.fontLigatures" = true;
+      "git.openRepositoryInParentFolders" = "always";
       "window.titleBarStyle" = "custom";
       "workbench.colorTheme" = "Gruvbox Dark Hard";
       "workbench.iconTheme" = "Monokai Classic Icons";
       "files.insertFinalNewline" = true;
       "files.trimFinalNewline" = true;
       "window.zoomLevel" = 4;
+      "gitlens.ai.experimental.generateCommitMessage.enabled" = false;
+      "gitlens.launchpad.indicator.enabled" = false;
+      "gitlens.launchpad.indicator.polling.enabled" = false;
+      "gitlens.plusFeatures.enabled" = false;
+      "gitlens.showWelcomeOnInstall" = false;
+      "gitlens.showWhatsNewAfterUpgrades" = false;
+      "gitlens.telemetry.enabled" = false;
+      "haskell.manageHLS" = "PATH";
+      "nix.enableLanguageServer" = true;
+      "nix.formatterPath" = "nixfmt";
+      "nix.serverPath" = "nil";
+      "nix.serverSettings" = {
+        "nil" = {
+          "formatting" = {
+            "command" = [
+              "nixfmt"
+            ];
+          };
+        };
+      };
+      "search.useGlobalIgnoreFiles" = true;
+      "search.useParentIgnoreFiles" = true;
+      "telemetry.telemetryLevel" = "off";
+      "terminal.integrated.fontSize" = 10;
+      "terminal.integrated.scrollback" = 10000;
+      "editor.lineNumbers" = "relative";
     };
   };
-
   # services.vscode-server = {
   #   enable = true;
   #   enableFHS = true;
