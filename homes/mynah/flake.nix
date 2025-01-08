f2b41009cf314007d3c21b07cd2ad8eba7a0f875
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2025-01-08 08:45:17 -0500

    start adding mynah

---
+++ homes/mynah/flake.nix
@@ -0,0 +1,128 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # neovim
+    neovim-flake.url = "github:nikita-sh/neovim-flake";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      neovim-flake,
+      home-manager,
+      nixpkgs,
+      ...
+    }:
+    let
+      configModule = {
+        config.vim = {
+          theme = {
+            enable = true;
+            style = "dark";
+            name = "gruvbox";
+          };
+          autocomplete = {
+            enable = true;
+          };
+          languages = {
+            terraform = {
+              enable = true;
+              treesitter.enable = true;
+            };
+            haskell = {
+              enable = true;
+              treesitter.enable = true;
+              lsp.enable = true;
+            };
+            nix = {
+              enable = true;
+              extraDiagnostics.enable = true;
+              format.enable = true;
+              lsp.enable = true;
+              treesitter.enable = true;
+            };
+            rust = {
+              enable = true;
+              crates = {
+                enable = true;
+                codeActions = true;
+              };
+              debugger.enable = true;
+              lsp.enable = true;
+            };
+            sql.enable = true;
+            ts.enable = true;
+            python = {
+              enable = true;
+              format.enable = true;
+              treesitter.enable = true;
+            };
+            markdown.enable = true;
+          };
+          filetree = {
+            nvimTreeLua = {
+              enable = true;
+              openTreeOnNewTab = true;
+              disableNetRW = true;
+            };
+          };
+          git = {
+            enable = true;
+            gitsigns.enable = true;
+          };
+          telescope = {
+            enable = true;
+          };
+          autopairs = {
+            enable = true;
+          };
+          statusline = {
+            lualine = {
+              enable = true;
+            };
+          };
+          visuals = {
+            enable = true;
+            cursorWordline.enable = true;
+            indentBlankline = {
+              enable = true;
+              eolChar = null;
+              fillChar = null;
+            };
+          };
+        };
+      };
+      pkgs = nixpkgs.legacyPackages.${system};
+      customNeovim = neovim-flake.lib.neovimConfiguration {
+        modules = [ configModule ];
+        inherit pkgs;
+      };
+
+      system = "x86-64-linux";
+    in
+    {
+      packages.${system}.neovim = customNeovim;
+
+      homeConfigurations."nikita@mynah" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+	        ../modules/mynah.nix
+	        {
+	          home = {
+	            homeDirectory = "/home/nikita";
+	            stateVersion = "24.05";
+	            username = "nikita";
+	          };
+	        }
+	      ];
+      };
+    };
+}
---
+++ systems/mynah/flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/release-24.05";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nixos-wsl,
+      ...
+    }:
+    {
+      nixosConfigurations.mynah = nixpkgs.lib.nixosSystem {
+        system = "x86-64-linux";
+        specialArgs = {
+          inherit inputs;
+	        inherit (inputs) self nixpkgs;
+          hostname = "mynah";
+        };
+        modules = [
+	        inputs.vscode-server.nixosModules.default
+          nixos-wsl.nixosModules.wsl
+          ./hardware.nix
+	        ../modules/bootloader.nix
+	        ../modules/network.nix
+	        ../modules/program.nix
+	        ../modules/services.nix
+	        ../modules/system.nix
+          ../modules/security.nix
+        ];
+      };
+    };
+}
