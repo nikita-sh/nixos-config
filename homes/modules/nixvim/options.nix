f232ffc32ce1a373382370eb14b120c31b9dbb95
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2025-01-14 12:07:50 -0500

    add mappings

--- homes/modules/nixvim/options.nix
+++ homes/modules/nixvim/options.nix
@@ -2,4 +2,12 @@
   colorschemes = {
     gruvbox.enable = true;
   };
+  globals.mapleader = ";";
+  autoCmd = [
+    {
+      event = [ "VimEnter" ];
+      callback = { __raw = "function() if vim.fn.argv(0) == '' then require('telescope.builtin').find_files() end end"; };
+    }
+  ];
+
 }
