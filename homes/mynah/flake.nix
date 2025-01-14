0e62172ee4b63992268d9d47e568cf4a9c9f4a46
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2025-01-14 11:26:42 -0500

    use nixvim

--- homes/mynah/flake.nix
+++ homes/mynah/flake.nix
@@ -5,124 +5,56 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # neovim
-    neovim-flake.url = "github:nikita-sh/neovim-flake";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
   outputs =
     inputs@{
       self,
-      neovim-flake,
+      nixvim,
       home-manager,
       nixpkgs,
       ...
     }:
     let
-      configModule = {
-        config.vim = {
-          theme = {
-            enable = true;
-            style = "dark";
-            name = "gruvbox";
-          };
-          autocomplete = {
-            enable = true;
-          };
-          languages = {
-            terraform = {
-              enable = true;
-              treesitter.enable = true;
-            };
-            haskell = {
-              enable = true;
-              treesitter.enable = true;
-              lsp.enable = true;
-            };
-            nix = {
-              enable = true;
-              extraDiagnostics.enable = true;
-              format.enable = true;
-              lsp.enable = true;
-              treesitter.enable = true;
-            };
-            rust = {
-              enable = true;
-              crates = {
-                enable = true;
-                codeActions = true;
-              };
-              debugger.enable = true;
-              lsp.enable = true;
-            };
-            sql.enable = true;
-            ts.enable = true;
-            python = {
-              enable = true;
-              format.enable = true;
-              treesitter.enable = true;
-            };
-            markdown.enable = true;
-          };
-          filetree = {
-            nvimTreeLua = {
-              enable = true;
-              openTreeOnNewTab = true;
-              disableNetRW = true;
-            };
-          };
-          git = {
-            enable = true;
-            gitsigns.enable = true;
-          };
-          telescope = {
-            enable = true;
-          };
-          autopairs = {
-            enable = true;
-          };
-          statusline = {
-            lualine = {
-              enable = true;
-            };
-          };
-          visuals = {
-            enable = true;
-            cursorWordline.enable = true;
-            indentBlankline = {
-              enable = true;
-              eolChar = null;
-              fillChar = null;
-            };
-          };
-        };
-      };
-      pkgs = nixpkgs.legacyPackages.${system};
-      customNeovim = neovim-flake.lib.neovimConfiguration {
-        modules = [ configModule ];
+      pkgs = import nixpkgs { inherit system; };
+      nixvimLib = nixvim.lib.${system};
+      nixvim' = nixvim.legacyPackages.${system};
+      nixvimModule = {
         inherit pkgs;
+	module = import ../modules/nixvim;
       };
-
-      system = "x86-64-linux";
+      nvim = nixvim'.makeNixvimWithModule nixvimModule;
+      system = "x86_64-linux";
     in
     {
-      packages.${system}.neovim = customNeovim;
-
       homeConfigurations."nikita@mynah" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         extraSpecialArgs = {
           inherit inputs;
         };
         modules = [
-	        ../modules/mynah.nix
-	        {
-	          home = {
-	            homeDirectory = "/home/nikita";
-	            stateVersion = "24.05";
-	            username = "nikita";
-	          };
-	        }
-	      ];
+	  ../modules/mynah.nix
+	  {
+	    home = {
+	      homeDirectory = "/home/nikita";
+	      stateVersion = "24.05";
+	      username = "nikita";
+	    };
+	  }
+	];
+      };
+
+      checks.${system} = {
+        default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+      };
+
+      packages.${system} = {
+        neovim = nvim;
       };
     };
 }
--- systems/kolibri/flake.nix
+++ systems/kolibri/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/release-24.05";
+    nixpkgs.url = "nixpkgs/release-24.11";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
@@ -19,17 +19,17 @@
           hostname = "kolibri";
         };
         modules = [
-	        inputs.vscode-server.nixosModules.default
+	  inputs.vscode-server.nixosModules.default
           ./hardware.nix
-	        ../modules/bootloader.nix
-	        ../modules/network.nix
-	        ../modules/program.nix
-	        ../modules/services.nix
-	        ../modules/system.nix
-	        ../modules/office-vpn.nix
-	        ../modules/software-workstation.nix
-	        ../modules/probe-rs.nix
-	        ../modules/vpn.nix
+	  ../modules/bootloader.nix
+	  ../modules/network.nix
+	  ../modules/program.nix
+	  ../modules/services.nix
+	  ../modules/system.nix
+	  ../modules/office-vpn.nix
+	  ../modules/software-workstation.nix
+	  ../modules/probe-rs.nix
+	  ../modules/vpn.nix
           ../modules/security.nix
         ];
       };
