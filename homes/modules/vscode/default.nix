ceffc29bc154386f0df7d7fa37222526958d15ce
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2025-02-04 10:58:34 -0500

    use clippy for rust-analyzer check

--- homes/modules/vscode/default.nix
+++ homes/modules/vscode/default.nix
@@ -60,6 +60,7 @@
       "terminal.integrated.scrollback" = 10000;
       "editor.lineNumbers" = "relative";
       "rust-analyzer.cargo.features" = "all";
+      "rust-analyzer.check.command" = "clippy";
       # "rust-analyzer.procMacro.enable" = false;
     };
   };
