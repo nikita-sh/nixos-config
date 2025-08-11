e74ccc35485b3160d8addf44ca50677d9e076b11
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2025-08-11 11:20:15 -0400

    update flake

--- homes/falke/flake.nix
+++ homes/falke/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
