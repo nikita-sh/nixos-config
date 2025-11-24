b7a950207f1f6689c4da9fc990e697c5f3d32c0c
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2025-11-24 09:48:54 -0500

    move nixvim lsp options to new module

--- homes/modules/nixvim/lsp.nix
+++ homes/modules/nixvim/lsp.nix
@@ -1,22 +1,17 @@
 {
-  plugins = {
-    lsp = {
-      enable = true;
-      servers = {
-        ts_ls.enable = true;
-        pyright.enable = true;
-        marksman.enable = true;
-        nil_ls.enable = true;
-        dockerls.enable = true;
-        bashls.enable = true;
-        clangd.enable = true;
-        ghcide.enable = true;
-        rust_analyzer = {
-          enable = true;
-          installCargo = true;
-          installRustc = true;
-        };
+  lsp = {
+    servers = {
+      ts_ls.enable = true;
+      pyright.enable = true;
+      marksman.enable = true;
+      nil_ls.enable = true;
+      dockerls.enable = true;
+      bashls.enable = true;
+      clangd.enable = true;
+      ghcide.enable = true;
+      rust_analyzer = {
+        enable = true;
       };
-    };      
-  }; 
+    };
+  };      
 }
