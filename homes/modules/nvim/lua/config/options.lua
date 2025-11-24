64fc88a1739fed2b4dd89249b8e95f98e5ce35c1
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2025-11-24 08:24:02 -0500

    bump flakes

---
+++ homes/modules/nvim/lua/config/options.lua
@@ -0,0 +1,36 @@
+-- global variables
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = false
+
+-- options
+vim.o.number = true
+vim.o.mouse = 'a'
+vim.o.showmode = false
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.signcolumn = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.inccommand = 'split'
+vim.o.confirm = true
+
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open Diagnostic [Q]uickfix List' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
